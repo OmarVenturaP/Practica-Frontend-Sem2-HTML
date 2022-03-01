8f987d7cb1da9c98011e4d022fd4b66197454f45
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-03-01 01:58:28 -0600

    Añadiendo JS

--- js/function.js
+++ js/function.js
@@ -1,9 +1,19 @@
+let enviarHTML = '';
 
+function guardarDatos() {
 
+  let miNombre = document.getElementById("nombre").value;
+  let miDireccion = document.getElementById("direccion").value;
+  let miTelefono = document.getElementById("telefono").value;
+  let miCorreo = document.getElementById("correo").value;
+  let miEntrega = document.querySelector('input[name="entrega"]:checked').value;
+  let miBetun = document.getElementById("betun").value;
+  let miDescripcion = document.getElementById("descripcion").value;
+  let miTopping = document.querySelectorAll('input[class="topping"]:checked').value;
+  
+  
+  enviarHTML += `<tr><b>Nombre: </b>${ miNombre }</tr><tr><b>Direccion: </b>${ miDireccion }</tr><tr><b>Telefono: </b>${ miTelefono }</tr><tr><b>Correo: </b>${ miCorreo }</tr><tr><b>Tipo de Entrega: </b>${ miEntrega }</tr><tr><b>Betun Elegido: </b>${ miBetun }</tr><tr><b>Topping Elegido: </b>${ miTopping }</tr><tr><b>Descripcion: </b>${ miDescripcion }</tr>`;
 
-function miFuncionCaptura() {
-let miDatoInput = document.getElementById("midato").value;
-let miDivHTML = '<p> ${miDatoInput} </p>';
-document.getElementById("miDivRecibeInfo").innerHTML = miDivHTML;
-
+  // Enviar al front
+  document.getElementById("valores").innerHTML = enviarHTML;
 }
