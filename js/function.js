7572db108d6dfe6fccd85be2d66f145eeccd9d3e
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-02-28 21:32:35 -0600

    add .js
    Add file JS

---
+++ js/function.js
@@ -0,0 +1,9 @@
+
+
+
+function miFuncionCaptura() {
+let miDatoInput = document.getElementById("midato").value;
+let miDivHTML = '<p> ${miDatoInput} </p>';
+document.getElementById("miDivRecibeInfo").innerHTML = miDivHTML;
+
+}
