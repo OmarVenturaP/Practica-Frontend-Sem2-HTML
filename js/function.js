2f457091dc75a333adfb88e167bd2c16f61dcef7
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-03-03 12:20:31 -0600

    Add Styles

--- js/function.js
+++ js/function.js
@@ -1,3 +1,6 @@
+
+// Tabla de datos de Pedido Pasteleria
+
 let enviarHTML = '';
 
 function guardarDatos() {
@@ -14,6 +17,17 @@ function guardarDatos() {
   
   enviarHTML += `<tr><b>Nombre: </b>${ miNombre }</tr><tr><b>Direccion: </b>${ miDireccion }</tr><tr><b>Telefono: </b>${ miTelefono }</tr><tr><b>Correo: </b>${ miCorreo }</tr><tr><b>Tipo de Entrega: </b>${ miEntrega }</tr><tr><b>Betun Elegido: </b>${ miBetun }</tr><tr><b>Topping Elegido: </b>${ miTopping }</tr><tr><b>Descripcion: </b>${ miDescripcion }</tr>`;
 
-  // Enviar al front
+  // Enviar al front en Página de Pedidos
   document.getElementById("valores").innerHTML = enviarHTML;
 }
+
+
+// Transicion de Boton Menu para Dispositivos mobiles
+
+const navButton = document.querySelector(".nav_button")
+const navUl = document.querySelector(".nav_ul")
+
+navButton.addEventListener("click", () => {
+  navUl.classList.toggle("nav_ul_visible");
+
+});
